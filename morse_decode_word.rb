a8193682a8ff398dd3d78c7a149dc65d9c07577f
zbahati <bahatizirimwabagabo@gmai.com>
2023-07-26 16:27:36 +0200

    Ft morse_decoder
    -ADD morse_decode_sentences that decodes message
    -ADD morse_decode_word that decorde message without space
    -ADD morse_decoder that decodes a letter
    -Correct rubocop linters error
    -Add Readme files
    
    # Finishes ..

--- morse_decode_word.rb
+++ morse_decode_word.rb
@@ -1,43 +1,20 @@
 def decoder_a(morse_char)
   morse_decoder = {
-    '.-' => 'A',
-    '-...' => 'B',
-    '-.-.' => 'C',
-    '-..' => 'D',
-    '.' => 'E',
-    '..-.' => 'F',
-    '--.' => 'G',
-    '....' => 'H',
-    '..' => 'I',
-    '.---' => 'J',
-    '-.-' => 'K',
-    '.-..' => 'L',
-    '--' => 'M',
-    '-.' => 'N',
-    '---' => 'O',
-    '.--.' => 'P',
-    '--.-' => 'Q',
-    '.-.' => 'R',
-    '...' => 'S',
-    '-' => 'T',
-    '..-' => 'U',
-    '...-' => 'V',
-    '.--' => 'W',
-    '-..-' => 'X',
-    '-.--' => 'Y',
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+    '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+    '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+    '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
     '--..' => 'Z'
   }
 
-  morse_word = morse_char.split(' ')
+  morse_word = morse_char.split
   decoded_word = decode_morse_word(morse_word, morse_decoder)
-  decoded_word.join('')
+  decoded_word.join
 end
 
 def decode_morse_word(morse_word, morse_decoder)
-  decoded_word = morse_word.map do |morse_sequence|
-    morse_decoder[morse_sequence]
-  end
-  decoded_word
+  morse_word.map { |morse_sequence| morse_decoder[morse_sequence] }
 end
 
 puts decoder_a('-- -.--   -. .- -- .')
